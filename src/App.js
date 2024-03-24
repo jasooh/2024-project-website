5157a9fa751626e50a9436fd4d8e9294950e4f52
Justin Abuyuan <abuyuanjustin@gmail.com>
2024-03-24 01:33:58 -0400

    Adjusted scale and changed routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import theme from './const/Theme';
 
